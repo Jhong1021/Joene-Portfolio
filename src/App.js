9bd33ab5054265cb1c5318a730e8c2148942709a
joenetuban <joenetuban@gmail.com>
2024-08-19 09:23:31 +0800

    about

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './index.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Projects from './components/Projects';
+import About from './components/About';
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
       <Projects />
+      <About />
     </div>
   )
 }
