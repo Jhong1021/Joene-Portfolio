77d8a430c6fe4a87fdf8cead5e314806d8112c6a
joenetuban <joenetuban@gmail.com>
2024-09-02 12:15:22 +0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-// import Sidebar from "./components/Sidebar";
-import Home from "./components/Home";
-import Projects from "./components/Projects";
-import About from "./components/About";
-import Tools from "./components/Tools";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import "./App.css";
-import "./index.css";
-
-function App() {
-  return (
-    <div className="App">
-=======
-import React from 'react'
+import React from 'react';
 import './App.css';
 import './index.css';
 import Navbar from './components/Navbar';
@@ -30,13 +12,11 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <div>
->>>>>>> 860d077ad12e086a8eb55c502443a4f603030ec5
       <Navbar />
       <Home />
       <Projects />
       <About />
-<<<<<<< HEAD
-      <Tools />
+      <Skills /> {/* Replaced Tools with Skills */}
       <Contact />
       <Footer />
     </div>
@@ -44,13 +24,3 @@ const App = () => {
 }
 
 export default App;
-=======
-      <Skills />
-      <Contact />
-      <Footer />
-    </div>
-  )
-}
-
-export default App
->>>>>>> 860d077ad12e086a8eb55c502443a4f603030ec5
